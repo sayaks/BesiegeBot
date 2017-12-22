4820cfd0946f87b2e6d5ec32d94c1d409a737023
ITR <ITR@live.no>
2017-12-22 01:48:44 +0100

    Made metabot reset on error too, but wait 5 minutes inbetween

--- metabot.py
+++ metabot.py
@@ -1,7 +1,10 @@
 import subprocess
+import time
 
 while True:
-	subprocess.run(["git","pull","origin","master"])
-	result = subprocess.run(["python3", "sourcecube.py"])
-	if result.returncode != 0:
-		break
+	while True:
+		subprocess.run(["git","pull","origin","master"])
+		result = subprocess.run(["python3", "sourcecube.py"])
+		if result.returncode != 0:
+			break
+	time.sleep(5*60)
