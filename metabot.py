a36930f2513cb0a3f9c0e14aaf1535f88a8bc360
ITR <ITR@live.no>
2017-12-22 16:52:04 +0100

    Made metabot use python3.6 instead of python3

--- metabot.py
+++ metabot.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 	while True:
 		while True:
 			subprocess.run(["git","pull","origin","master"])
-			result = subprocess.run(["python3", "sourcecube.py"])
+			result = subprocess.run(["python3.6", "sourcecube.py"])
 			if result.returncode != 0:
 				break
 		time.sleep(5*60)
