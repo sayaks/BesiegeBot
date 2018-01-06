dd5b921a9f5a61ea83693279f1b5b51959834006
ITR <ITR@live.no>
2018-01-06 01:10:43 +0100

    Made metabot always reload in the folder it's in

--- metabot.py
+++ metabot.py
@@ -1,12 +1,24 @@
 import subprocess
 import time
+import os
 
 if __name__ == '__main__':
+	path = os.path.dirname(os.path.realpath(__file__))
+	print(path)
 	while True:
 		while True:
-			subprocess.run(["git", "fetch", "--all"])
-			subprocess.run(["git", "reset", "--hard", "origin/master"])
-			result = subprocess.run(["python3.6", "sourcecube.py"])
+			subprocess.run(
+				["git", "fetch", "--all"], 
+				cwd=path
+			)
+			subprocess.run(
+				["git", "reset", "--hard", "origin/master"], 
+				cwd=path
+			)
+			result = subprocess.run(
+				["python3.6", "sourcecube.py"], 
+				cwd=path
+			)
 			if result.returncode != 0:
 				break
 		time.sleep(5 * 60)
