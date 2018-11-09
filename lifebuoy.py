cba71c071dd76d23ad022626271f95005a55abd7
ITR <ITR@live.no>
2018-11-09 23:08:09 +0100

    Added spacing inbetween logs

--- lifebuoy.py
+++ lifebuoy.py
@@ -2,18 +2,33 @@ import mundane
 import time
 
 
-save_interval = 60*10
+save_interval = 10 * 60
 next_save = time.time() + save_interval
 
+log_interval = 2.25 * 60 * 60
+next_logged_save = 0
+unlogged_saves = 0
 
 def save_if_needed(client):
 	global next_save
 	if time.time() >= next_save:
 		next_save = time.time() + save_interval
 		mundane.save_all()
-		client.log(
-			f'Executed regular save at interval'
-		)
+		
+		if time.time() > next_logged_save:
+			last_logged_save = time.time + log_interval
+			if unlogged_saves > 0:
+				client.log(
+					f'Executed regular save at interval:\n'
+					f'({unlogged_saves} unlogged saves)'
+				)
+				unlogged_saves = 0
+			else:
+				client.log(
+					f'Executed regular save at interval'
+				)
+		else:
+			unlogged_saves += 1
 
 		
 async def force_save(client, message, prefix):
